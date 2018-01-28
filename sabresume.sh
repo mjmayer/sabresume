cad7a59dcf4893019ba5ec702eed6da3a0828f55
Michael Mayer <mjmayer@gmail.com>
2018-01-27 20:46:17 -0800

    Allow insucure ssl

--- sabresume.sh
+++ sabresume.sh
@@ -5,15 +5,15 @@ SABPORT="${SABPORT:-8080}"
 PROT="${PROT:-http}"
 SPACELIMIT="${SPACELIMIT:-20}"
 SLEEP="${SLEEP:-300}"
-if  ! curl -L -s  "$PROT://${SABHOST}:${SABPORT}/api?apikey=${SABKEY}&output=json&mode=fullstatus" | grep -q 'pid'; then
+if  ! curl -L -s --insecure "$PROT://${SABHOST}:${SABPORT}/api?apikey=${SABKEY}&output=json&mode=fullstatus" | grep -q 'pid'; then
   echo "Failed to connect to: $PROT://$SABHOST:$SABPORT using apikey $SABKEY"
   exit
 fi
-PAUSED=$(curl -s -L "$PROT://${SABHOST}:${SABPORT}/api?apikey=${SABKEY}&output=json&mode=queue&search=paused" | jq -r '.["queue"]["paused"]')
-FREESPACE=$(curl -s -L "$PROT://${SABHOST}:${SABPORT}/api?apikey=${SABKEY}&output=json&mode=queue&search=paused" | jq -r '.["queue"]["diskspace1"]')
+PAUSED=$(curl -s -L --insecure "$PROT://${SABHOST}:${SABPORT}/api?apikey=${SABKEY}&output=json&mode=queue&search=paused" | jq -r '.["queue"]["paused"]')
+FREESPACE=$(curl -s -L --insecur "$PROT://${SABHOST}:${SABPORT}/api?apikey=${SABKEY}&output=json&mode=queue&search=paused" | jq -r '.["queue"]["diskspace1"]')
 while true; do
    if  [ $(echo "$FREESPACE > $SPACELIMIT" | bc -l) == 1 ] && [ $PAUSED == 'true' ]; then
-      RESUMED=$(curl -s -L "$PROT://${SABHOST}:${SABPORT}/api?apikey=${SABKEY}&output=json&mode=resume" | jq -r '.["status"]')
+      RESUMED=$(curl -s -L --insecure "$PROT://${SABHOST}:${SABPORT}/api?apikey=${SABKEY}&output=json&mode=resume" | jq -r '.["status"]')
    fi
    if [ $RESUMED ]; then
      DATE=`date '+%Y-%m-%d %H:%M:%S'`
