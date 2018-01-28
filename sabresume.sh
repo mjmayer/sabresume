67c9a711415c4eb753d3a4559b8ae0e5a238352a
Michael Mayer <mjmayer@gmail.com>
2018-01-28 07:30:17 -0800

    Add variable for base uri

--- sabresume.sh
+++ sabresume.sh
@@ -5,15 +5,16 @@ SABPORT="${SABPORT:-8080}"
 PROT="${PROT:-http}"
 SPACELIMIT="${SPACELIMIT:-20}"
 SLEEP="${SLEEP:-300}"
-if  ! curl -L -s --insecure "$PROT://${SABHOST}:${SABPORT}/api?apikey=${SABKEY}&output=json&mode=fullstatus" | grep -q 'pid'; then
+BASE_URI="$PROT://${SABHOST}:${SABPORT}/api?apikey=${SABKEY}"
+if  ! curl -L -s --insecure "${BASE_URI}&output=json&mode=fullstatus" | grep -q 'pid'; then
   echo "Failed to connect to: $PROT://$SABHOST:$SABPORT using apikey $SABKEY"
   exit
 fi
-PAUSED=$(curl -s -L --insecure "$PROT://${SABHOST}:${SABPORT}/api?apikey=${SABKEY}&output=json&mode=queue&search=paused" | jq -r '.["queue"]["paused"]')
-FREESPACE=$(curl -s -L --insecur "$PROT://${SABHOST}:${SABPORT}/api?apikey=${SABKEY}&output=json&mode=queue&search=paused" | jq -r '.["queue"]["diskspace1"]')
+PAUSED=$(curl -s -L --insecure "${BASE_URI}&output=json&mode=queue&search=paused" | jq -r '.["queue"]["paused"]')
+FREESPACE=$(curl -s -L --insecur "${BASE_URI}&output=json&mode=queue&search=paused" | jq -r '.["queue"]["diskspace1"]')
 while true; do
    if  [ $(echo "$FREESPACE > $SPACELIMIT" | bc -l) == 1 ] && [ $PAUSED == 'true' ]; then
-      RESUMED=$(curl -s -L --insecure "$PROT://${SABHOST}:${SABPORT}/api?apikey=${SABKEY}&output=json&mode=resume" | jq -r '.["status"]')
+      RESUMED=$(curl -s -L --insecure "${BASE_URI}&output=json&mode=resume" | jq -r '.["status"]')
    fi
    if [ $RESUMED ]; then
      DATE=`date '+%Y-%m-%d %H:%M:%S'`
